ed0ac6cb9ae2e461b226ed3e3c15d830a74a9d38
hayato94087 <hayato94087@gmail.com>
2023-07-25 06:36:07 +0900

    完成版

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <main className="text-lg">
-      テストページ
+    <main>
+      {/* サイズ指定 */}
+      <span className="i-lucide-cat"></span>
+      <span className="i-lucide-cat w-5 h-5"></span>
+      <span className="i-lucide-cat w-7 h-7"></span>
+      <br/>
+      {/* 色を指定 */}
+      <span className="i-lucide-cat bg-blue-500"></span>
+      <span className="i-lucide-cat bg-red-500"></span>
+      <span className="i-lucide-cat bg-orange-500"></span>
+      <br/>
+      {/* アニメーションを追加 */}
+      <span className="i-lucide-cat animate-spin"></span>
+      <span className="i-lucide-cat animate-bounce"></span>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const {
+  iconsPlugin,
+  getIconCollections,
+} = require("@egoist/tailwindcss-icons");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [],
+  plugins: [
+    iconsPlugin({
+      // 利用したい icon collection を利用する
+      // https://icones.js.org/
+      collections: getIconCollections(["tabler", "lucide"]),
+    }),
+  ],
 };
